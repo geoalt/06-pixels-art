d30ff49a9320bcdb8424ed21f5c2150f2abbe43f
Diogo Martins <me.martinsfe@gmail.com>
2022-08-02 16:48:13 -0300

    chore: update

--- script.js
+++ script.js
@@ -3,25 +3,23 @@ console.log(accessColorDiv);
 const accessBoardPixels = document.querySelector('#pixel-board');
 console.log(accessBoardPixels);
 
-
-const getColor = event => {
-  let savedColor;
+const getColor = (event) => {
+  let savedColor = '';
   savedColor = getComputedStyle(event.target).backgroundColor;
   sessionStorage.setItem('color', JSON.stringify(savedColor));
-}
+};
 
-
-const receiveColor = event => {
-  let changeColor = JSON.parse(sessionStorage.getItem('color'));
+const receiveColor = (event) => {
+  const changeColor = JSON.parse(sessionStorage.getItem('color'));
   event.target.style.backgroundColor = changeColor;
-}
+};
 
-for (let i of accessColorDiv) {
+for (const i of accessColorDiv) {
   i.addEventListener('click', getColor);
 }
 
 // Cria a Board Pixels
-let boardSize = 15;
+let boardSize = 5;
 for (let rows = 0; rows < boardSize; rows += 1) {
   const createDiv = document.createElement('div');
   accessBoardPixels.insertBefore(createDiv, accessBoardPixels.lastElementChild);
